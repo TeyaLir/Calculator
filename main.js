4774ff6a613a8e4681fef3a9364363c7c8c6265c
TeyaLir <ia_88@bk.ru>
2022-01-22 14:02:08 +0300

    Add project

---
+++ main.js
@@ -0,0 +1,44 @@
+// let a = +prompt('Введите первое число', '');
+// let operator = prompt('Введите математическое действие', '');
+// let b = +prompt('Введите второе число', '');
+
+function calc(a, operator, b) {
+  if (typeof a !== 'number' || typeof b !== 'number' || operator === undefined) {
+    return 'Ошибка: Указаны не все параметры или они не равны числу.';
+  }
+
+  switch (operator) {
+    case "+":
+      return (a + b);
+      break;
+
+    case "-":
+      return (a - b);
+      break;
+
+    case "*":
+      return (a * b);
+      break;
+
+    case "/":
+      if (b === 0) {
+        return ("Делить на ноль нельзя");
+      } else {
+        return (a / b);
+      }
+      break;
+
+    default:
+      return ("Неизвестный оператор");
+  }
+}
+
+console.log(calc(3, '+', 5));
+console.log(calc(10, '-', 6));
+console.log(calc(2, '*', 7));
+console.log(calc(20, '/', 7));
+console.log(calc(16, '/', 0));
+console.log(calc(5, '', 7));
+console.log(calc('a', '-', 7));
+
+// alert(calc(a, operator, b));

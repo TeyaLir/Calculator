813d78f4c6ee5f0ee5b27625956d2e198b8e7c73
TeyaLir <ia_88@bk.ru>
2022-01-23 13:30:46 +0300

    Change the code

--- main.js
+++ main.js
@@ -3,29 +3,38 @@
 // let b = +prompt('Введите второе число', '');
 
 function calc(a, operator, b) {
-  if (typeof a !== 'number' || typeof b !== 'number' || operator === undefined) {
+  const isValid = typeof a == 'number' && typeof b == 'number' && operator !== undefined;
+
+  if (isValid) {
+    switch (operator) {
+      case "+":
+        return (a + b);
+
+      case "-":
+        return (a - b);
+
+      case "*":
+        return (a * b);
+
+      case "/":
+        if (b === 0) {
+          return ("Делить на ноль нельзя");
+        } else {
+          return (a / b);
+        }
+
+      case "%":
+        return (a % b);
+
+      case "**":
+        return (a ** b);
+
+      default:
+        return ("Неизвестный оператор");
+    }
+
+  } else {
     return 'Ошибка: Указаны не все параметры или они не равны числу.';
-  }
-
-  switch (operator) {
-    case "+":
-      return (a + b);
-
-    case "-":
-      return (a - b);
-
-    case "*":
-      return (a * b);
-
-    case "/":
-      if (b === 0) {
-        return ("Делить на ноль нельзя");
-      } else {
-        return (a / b);
-      }
-
-    default:
-      return ("Неизвестный оператор");
   }
 }
 
@@ -34,7 +43,9 @@ console.log(calc(10, '-', 6));
 console.log(calc(2, '*', 7));
 console.log(calc(20, '/', 7));
 console.log(calc(16, '/', 0));
+console.log(calc(35, '%', 3));
+console.log(calc(2, '**', 4));
 console.log(calc(5, '', 7));
-console.log(calc('a', '-', 7));
+console.log(calc('a', '-', 9));
 
 // alert(calc(a, operator, b));

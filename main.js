fafcd8045b37bd69a93ef745d377bb568cd864a3
TeyaLir <ia_88@bk.ru>
2022-01-26 23:34:20 +0300

    Add object in project

--- main.js
+++ main.js
@@ -1,51 +1,31 @@
-// let a = +prompt('Введите первое число', '');
-// let operator = prompt('Введите математическое действие', '');
-// let b = +prompt('Введите второе число', '');
-
 function calc(a, operator, b) {
-  const isValid = typeof a == 'number' && typeof b == 'number' && operator !== undefined;
+  const isValid = 
+    typeof a == 'number' && 
+    typeof b == 'number' && 
+    operator !== undefined;
+
+  const operations = {
+    sub: a - b,
+    sum: a + b,
+    mult: a * b,
+    div: a / b
+  };
 
   if (isValid) {
-    switch (operator) {
-      case "+":
-        return (a + b);
-
-      case "-":
-        return (a - b);
-
-      case "*":
-        return (a * b);
-
-      case "/":
-        if (b === 0) {
-          return ("Делить на ноль нельзя");
-        } else {
-          return (a / b);
-        }
-
-      case "%":
-        return (a % b);
-
-      case "**":
-        return (a ** b);
-
-      default:
-        return ("Неизвестный оператор");
+    if (operator in operations) {
+      return operations[operator];
+    } else {
+      return "Unknow operation";
     }
-
   } else {
-    return 'Ошибка: Указаны не все параметры или они не равны числу.';
+    return "Error";
   }
 }
 
-console.log(calc(3, '+', 5));
-console.log(calc(10, '-', 6));
-console.log(calc(2, '*', 7));
-console.log(calc(20, '/', 7));
-console.log(calc(16, '/', 0));
-console.log(calc(35, '%', 3));
-console.log(calc(2, '**', 4));
+console.log(calc(3, 'sum', 5));
+console.log(calc(10, 'sub', 6));
+console.log(calc(2, 'mult', 7));
+console.log(calc(20, 'div', 7));
+console.log(calc(16, 'div', 0));
 console.log(calc(5, '', 7));
-console.log(calc('a', '-', 9));
-
-// alert(calc(a, operator, b));
+console.log(calc('a', 'sub', 9));
